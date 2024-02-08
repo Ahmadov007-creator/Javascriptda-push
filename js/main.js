66dc9e7ae0d0b4f6e8be6ccfe968228914ef4e74
Ahmadov007-creator <abdulazizahmadov2007@gmail.com>
2024-02-08 19:50:41 +0500

    This site created by Akhmadov

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 function addText() {
-  var text = document.getElementById("wordInput").value;
-  var elInp = document.getElementById("wordInput");
-  var position = document.getElementById("positionInput").value;
-  var h1Element = document.getElementById("text");
+    var text = document.getElementById("wordInput").value;
+    var elInp = document.getElementById("wordInput");
+    var position = document.getElementById("positionInput").value;
+    var h1Element = document.getElementById("text");
 
-  if (position.toLowerCase() === "boshiga") {
-      h1Element.textContent = text + " " + h1Element.textContent;
-  } else if (position.toLowerCase() === "ohiriga") {
-      h1Element.textContent += " " + text;
-  } else {
-      alert("Noto'g'ri so'z kiritildi. Iltimos, 'boshiga' yoki 'ohiriga'ni kiriting.");
-  }
-  elInp.value = ''
-  elInp.focus()
+    if (position.toLowerCase() === "boshiga") {
+        h1Element.textContent = text + " " + h1Element.textContent;
+    } else if (position.toLowerCase() === "ohiriga") {
+        h1Element.textContent += " " + text;
+    } else {
+        alert("Noto'g'ri so'z kiritildi. Iltimos, 'boshiga' yoki 'ohiriga'ni kiriting.");
+    }
+    elInp.value = ''
+    elInp.focus()
 }
+setTimeout(() => {
+    let loader = document.queryader_boSelector('.lox')
+    loader.style.height = 0
+}, 4000)
+
+setTimeout(() => {
+    let loader = document.querySelector('.loader_box')
+    loader.style.display = "none"
+}, 4000)
